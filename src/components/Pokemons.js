231fa4a602426f8a61e0a5f97acbf100beb1db5d
evinracher <eevinley@gmail.com>
2020-09-16 21:35:38 -0500

    Finishing listing pokemons

--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -3,60 +3,43 @@ import Card from './Card';
 import styles from '../styles/Pokemons.module.css';
 import { connect } from 'react-redux';
 import { updatePokemons, load } from '../redux/actions/globalActions';
+import useInfiniteScroll from 'react-infinite-scroll-hook';
+import { SyncLoader } from 'react-spinners';
 
 const Pokemons = (props) => {
   const { currURL, nextURL, pokemons, isLoading } = props; // attributes
   const { update, load } = props; // functions
+  const hasNextPage = props.nextURL !== null ? true : false;
 
   useEffect(() => {
-    console.log('Charging pokemons: ' + nextURL);
     update(currURL);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currURL])
 
-  // TODO: Delete the observer
-  useEffect(() => {
-    console.log('initial url: ' + nextURL);
-    var options = {
-      root: null,
-      threshold: 0.1
-    };
-    // initialize IntersectionObserver
-    // and attaching to Load More div
-    const observer = new IntersectionObserver(handleObserver, options);
-    if (loader.current) {
-      observer.observe(loader.current)
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const loader = useRef();
-
-  const handleObserver = (entities) => {
-    const target = entities[0];
-    if (target.isIntersecting) {
-      if (!isLoading) {
-        console.log("intersecting")
-        load()
-      }
-    }
+  function handleLoadMore() {
+    load();
   }
+
+  const infiniteRef = useInfiniteScroll({
+    loading: isLoading,
+    hasNextPage,
+    onLoadMore: handleLoadMore,
+  });
 
   return (
     <div className={styles['pokemons-container']}>
-      <div className={styles['pokemons']}>
+      <div className={styles['pokemons']} ref={infiniteRef}>
         {
           pokemons.map((pokemon) => {
             return <Card key={pokemon.id} pokemon={pokemon} />
           })
         }
       </div>
-      <div className={styles['loader']} ref={loader}>
-        {/* loader */}
-        </div>
-      <div>
-        {isLoading && 'Loading...'}
+      <div className={styles['loader']}>
+        {isLoading && (<div>
+          <SyncLoader />
+          <p>Loading...</p>
+        </div>)}
       </div>
     </div>
   )
