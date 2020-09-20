ac6d6d02f5d3bfcb15c466145d73e73dbc69e5b3
evinracher <eevinley@gmail.com>
2020-09-19 21:00:22 -0500

    Deleting code

--- src/Utils.js
+++
@@ -1,9 +0,0 @@
-export function getClassNames(styles, classList) {
-  let res = "";
-  classList.forEach(classname => {
-    if (styles[classname]) {
-      res += " " + styles[classname];
-    }
-  });
-  return res;
-}
--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import global from './globalReducer'
-import pokemon from './pokemonReducer'
-
-export default combineReducers({
-  global,
-  pokemon
-})
