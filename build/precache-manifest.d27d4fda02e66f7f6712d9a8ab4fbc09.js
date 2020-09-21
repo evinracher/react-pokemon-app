b09228e9b42ee6f204b3982d09caf5aa1ea6e253
evinracher <eevinley@gmail.com>
2020-09-21 16:07:50 -0500

    Preparing the build

---
+++ build/precache-manifest.d27d4fda02e66f7f6712d9a8ab4fbc09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2b72a9248a549feaec0755158358ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "225cfc3e4cc065d2eaca",
+    "url": "./static/css/main.228ad90f.chunk.css"
+  },
+  {
+    "revision": "4ec1a72a9ca56a651be7",
+    "url": "./static/js/2.a4ad6b3b.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "./static/js/2.a4ad6b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225cfc3e4cc065d2eaca",
+    "url": "./static/js/main.6d4f32c4.chunk.js"
+  },
+  {
+    "revision": "c858bd39185cb52eb0fd",
+    "url": "./static/js/runtime-main.d205d416.js"
+  },
+  {
+    "revision": "50ab3c9fdfba9f167f872cadfb9df993",
+    "url": "./static/media/whos_that_pokemon.50ab3c9f.png"
+  }
+]);
