778c833dbb740b61452cd7ebbff417113ac080b5
evinracher <eevinley@gmail.com>
2020-09-21 11:54:06 -0500

    Addig docs

---
+++ docs/precache-manifest.c1bf76e8e653720d2766780ccbbf880b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2183df75907146cffc991293c8a1c4e7",
+    "url": "/pokemon-react-app/index.html"
+  },
+  {
+    "revision": "b473da1081a879a9736c",
+    "url": "/pokemon-react-app/static/css/main.228ad90f.chunk.css"
+  },
+  {
+    "revision": "4ec1a72a9ca56a651be7",
+    "url": "/pokemon-react-app/static/js/2.a4ad6b3b.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/pokemon-react-app/static/js/2.a4ad6b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b473da1081a879a9736c",
+    "url": "/pokemon-react-app/static/js/main.61fff905.chunk.js"
+  },
+  {
+    "revision": "b8e8594d946e04b30f7e",
+    "url": "/pokemon-react-app/static/js/runtime-main.5f339910.js"
+  },
+  {
+    "revision": "50ab3c9fdfba9f167f872cadfb9df993",
+    "url": "/pokemon-react-app/static/media/whos_that_pokemon.50ab3c9f.png"
+  }
+]);
